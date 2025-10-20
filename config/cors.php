f5f697edb5160aaaa186bdf983f3348a203e2703
walter <walteranyika@gmail.com>
2025-10-20 08:39:34 +0300

    Allow many origins

--- config/cors.php
+++ config/cors.php
@@ -8,6 +8,6 @@ return [
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'allowed_origins' => ['http://127.0.0.1:8001'],
+    'allowed_origins' => ['*'],
     'supports_credentials' => true,
 ];
