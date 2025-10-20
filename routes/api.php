6db1783d48475275c04382169d7a43341beb0380
walter <walteranyika@gmail.com>
2025-10-20 07:46:30 +0300

    Fix response

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\PrintController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/health', function (Request $request) {
+    return response()->json(["status"=>"Working"]);
+});
 
 Route::post('print-receipt', [PrintController::class, 'printReceipt'])->name('print-receipt');
